b90694e6c118f4ee585f351aae9bb23eab441d17
aauger <augera09@students.rowan.edu>
2018-09-21 23:48:51 -0400

    Few bug fixes, error handling to come

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,26 @@ use std::string::String;
 use std::path::Path;
 
 fn main() {
-    let THRESHOLD: u8 = 255;
+    let THRESHOLD :u8 = 255;
+    let INFILE_PATH : String = String::from("..\\fstore\\inf\\");
+    let OUTFILE_PATH : String = String::from("..\\fstore\\outf\\");
+
     let mut rframecount : u64 = 1;
     let mut oframecount : u64 = 1;
     println!("{:?}", std::env::current_dir().unwrap());
-    let fcount = std::fs::read_dir(Path::new("fstore\\inf")).unwrap().count();
+    let fcount = std::fs::read_dir(Path::new(INFILE_PATH)).unwrap().count();
     println!("count: {}", fcount);
 
-    for _ in 0..fcount{
+    for _ in 0..fcount-1{
         let mut fp = frame_pair::FramePair::new(
-            String::from(format!("fstore\\inf\\{}.png", rframecount)),
-            String::from(format!("fstore\\inf\\{}.png", rframecount+1))
+            String::from(format!("{}{}.png", INFILE_PATH, rframecount)),
+            String::from(format!("{}{}.png", INFILE_PATH, rframecount+1))
         );
         fp.generate_interframe(THRESHOLD);
         fp.save_aframe(Path::new
-            (&String::from(format!("fstore\\outf\\{}.png", oframecount))));
+            (&String::from(format!("{}{}.png", OUTFILE_PATH, oframecount))));
         fp.save_interframe(Path::new
-            (&String::from(format!("fstore\\outf\\{}.png", oframecount+1))));
+            (&String::from(format!("{}{}.png", OUTFILE_PATH, oframecount+1))));
         oframecount += 2;
         rframecount += 1;
         println!("{}/{}", oframecount, fcount*2);
