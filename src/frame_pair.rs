04c7b4cf578913c817ea6c0f6c6092cfa8497940
aauger <augera09@students.rowan.edu>
2018-09-26 14:52:41 -0400

    Small fixes & prep for error handling

--- src/frame_pair.rs
+++ src/frame_pair.rs
@@ -1,13 +1,13 @@
 extern crate image;
 use std::path::Path;
 use image::GenericImageView;
-use image::DynamicImage;
 use image::Rgba;
+use std::io;
 
 fn blend(a: u8, b: u8, thres: u8) -> u8
 {
-    let ia : i32 = a as i32;
-    let ib : i32 = b as i32;
+    let ia: i32 = a as i32;
+    let ib: i32 = b as i32;
     let diff = (ia-ib).wrapping_abs();
     if diff >= (thres as i32) {
         b
@@ -26,45 +26,47 @@ pub struct FramePair {
 impl FramePair {
     pub fn new(fna: String, fnb: String) -> Self
     {
-        let ima = image::open(Path::new(&fna)).unwrap();
-        let imb = image::open(Path::new(&fnb)).unwrap();
+        let ima = image::open(Path::new(&fna)).unwrap(); //FIXME
+        let imb = image::open(Path::new(&fnb)).unwrap(); //FIXME
         let iout = image::ImageBuffer::new(ima.width(), ima.height());
 
         FramePair { a: ima, b: imb, out: iout }
     }
 
-    pub fn generate_interframe(&mut self, thres : u8)
+    pub fn generate_iframe(&mut self, thres : u8)
     {
-        for x in 0..self.a.width() {
-            for y in 0..self.a.height(){
-                let acolor: Rgba<u8> = self.a.get_pixel(x, y);
-                let bcolor: Rgba<u8> = self.b.get_pixel(x, y);
-                let ncolor: Rgba<u8> = Rgba(
+        let (w,h) = self.a.dimensions();
+
+        for x in 0..w {
+            for y in 0..h {
+                let a_color: Rgba<u8> = self.a.get_pixel(x, y);
+                let b_color: Rgba<u8> = self.b.get_pixel(x, y);
+                let n_color: Rgba<u8> = Rgba(
                     [
-                        blend(acolor.data[0], bcolor.data[0], thres),
-                        blend(acolor.data[1], bcolor.data[1], thres),
-                        blend(acolor.data[2], bcolor.data[2], thres),
-                        blend(acolor.data[3], bcolor.data[3], thres)
+                        blend(a_color.data[0], b_color.data[0], thres), //R
+                        blend(a_color.data[1], b_color.data[1], thres), //G
+                        blend(a_color.data[2], b_color.data[2], thres), //B
+                        blend(a_color.data[3], b_color.data[3], thres)  //A
                     ]
                 );
-                self.out.put_pixel(x,y,ncolor);
+                self.out.put_pixel(x,y,n_color);
             }
         }
     }
 
-    pub fn save_aframe(&self, path : &Path)
+    pub fn save_aframe(&self, path : &Path) -> io::Result<()>
     {
-        self.a.save(path);
+        self.a.save(path)
     }
 
-    pub fn save_bframe(&self, path : &Path)
+    pub fn save_bframe(&self, path : &Path) -> io::Result<()>
     {
-        self.b.save(path);
+        self.b.save(path)
     }
 
-    pub fn save_interframe(&self, path : &Path)
+    pub fn save_iframe(&self, path : &Path) -> io::Result<()>
     {
-        self.out.save(path);
+        self.out.save(path)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,27 @@ use std::string::String;
 use std::path::Path;
 
 fn main() {
-    let THRESHOLD :u8 = 255;
-    let INFILE_PATH : String = String::from("..\\fstore\\inf\\");
-    let OUTFILE_PATH : String = String::from("..\\fstore\\outf\\");
+
+    let threshold :u8 = 255;
+    let infolder : String = String::from("..\\fstore\\inf\\");
+    let outfolder : String = String::from("..\\fstore\\outf\\");
 
     let mut rframecount : u64 = 1;
     let mut oframecount : u64 = 1;
     println!("{:?}", std::env::current_dir().unwrap());
-    let fcount = std::fs::read_dir(Path::new(INFILE_PATH)).unwrap().count();
+    let fcount = std::fs::read_dir(Path::new(&infolder)).unwrap().count(); //FIXME
     println!("count: {}", fcount);
 
     for _ in 0..fcount-1{
         let mut fp = frame_pair::FramePair::new(
-            String::from(format!("{}{}.png", INFILE_PATH, rframecount)),
-            String::from(format!("{}{}.png", INFILE_PATH, rframecount+1))
+            String::from(format!("{}{}.png", infolder, rframecount)),
+            String::from(format!("{}{}.png", infolder, rframecount+1))
         );
-        fp.generate_interframe(THRESHOLD);
+        fp.generate_iframe(threshold);
         fp.save_aframe(Path::new
-            (&String::from(format!("{}{}.png", OUTFILE_PATH, oframecount))));
-        fp.save_interframe(Path::new
-            (&String::from(format!("{}{}.png", OUTFILE_PATH, oframecount+1))));
+            (&String::from(format!("{}{}.png", outfolder, oframecount))));
+        fp.save_iframe(Path::new
+            (&String::from(format!("{}{}.png", outfolder, oframecount+1))));
         oframecount += 2;
         rframecount += 1;
         println!("{}/{}", oframecount, fcount*2);
